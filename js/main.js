cf2008677552c0eba474d36b1f503af0efb83a38
Marcio Douglas <dmarcio998@gmail.com>
2021-03-20 22:10:05 -0300

    atu

--- js/main.js
+++ js/main.js
@@ -1,2 +1,24 @@
-var nome  = window.prompt('Qual o seu nome?')
+/*var nome  = window.prompt('Qual o seu nome?')
 window.alert(`O seu nome é :${nome}`)
+
+var lista =["dc","dd","de"]
+lista.push("dn")
+lista.pop(lista)
+window.alert(lista.reverse())
+window.alert(lista.length())*/
+
+var idade = 0
+while (idade <= 5){
+alert(idade)
+idade++
+}
+/*
+if (idade >= 18){
+    alert('Maior de idade!')
+}else{
+    alert('Menor de idade')
+}
+*/
+
+/*var fruta = {nome : "maça",cor : "vermelha"}
+alert(fruta.nome)*/
